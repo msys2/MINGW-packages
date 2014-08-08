b5206ea3c7d00320aa030a1e1e90d1d492f336e6
Alexpux <alexey.pawlow@gmail.com>
2014-08-08 09:36:49 +0400

    pathtools: Fix declaration.

--- mingw-w64-pathtools/pathtools.h
+++ mingw-w64-pathtools/pathtools.h
@@ -40,6 +40,6 @@ strip_n_prefix_folders(char const * path, size_t n);
 void
 strip_n_suffix_folders(char * path, size_t n);
 
-char * get_relocated_path_list(char const * from_bindir, char const * topathlist);
+char * get_relocated_path_list(char const * from, char const * to_path_list);
 
 #endif /* PATHTOOLS_H */
