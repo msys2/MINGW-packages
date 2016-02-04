d904180abdd38ab9bf5e5a40695ac11b6bb5ddd1
Qian Hong <qhong@codeweavers.com>
2016-02-05 02:15:47 +0800

    drone: Fix build script. Introduce matrix builds.

--- drone/build.sh
+++ drone/build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+set -e
+
+git config --global user.email "ci@msys2.org"
+git config --global user.name "MSYS2 Build Bot"
+
 # fetch first changed file, assume at most one package touched per commit
-TOUCHED=`git show --pretty="format:" --name-only | grep . | head -1`
+TOUCHED=`git show -m --pretty="format:" --name-only | grep . | head -1`
 PKGDIR=`dirname $TOUCHED`
-if [ "$PKGDIR" = "." ]
+if [ "$PKGDIR" = "." || "$PKGDIR" = "drone" ]
 then
     echo Nothing to test
 else
     pushd $PKGDIR > /dev/null
-    makepkg-mingw -f -s --noconfirm --skippgpcheck --noprogressbar
+    if [ "$RUNTEST" = "true" ]
+    then
+        makepkg-mingw -f -s --noconfirm --skippgpcheck || true
+    else
+        makepkg-mingw -f -s --noconfirm --skippgpcheck --nocheck
+    fi
     popd > /dev/null
 fi
