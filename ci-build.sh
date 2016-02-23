5263a4ce04b784774e7131de53a329670583bffd
Renato Silva <br.renatosilva@gmail.com>
2016-02-23 11:51:36 -0300

    Improvements and fixes for CI script.
    * New implementation for detection of changed recipes.
      This is done by listing files changed since upstream master. When the list is
      empty (e.g. current branch is master itself), changes from latest commit are
      built instead.
    
    * Merge dependency installation and actual build into one single makepkg call.
      Extracting sources normally for each architecture on actual build should fix
      patches getting applied twice for MINGW packages.
    
    * Use --cleanbuild in makepkg call so MINGW packages using a common build
      directory across different architectures always rebuild for each.
    
    * Use --rmdeps in makepkg call so missing depends/makedepends can be detected
      for subsequent recipe builds.
    
    * Source code refactoring.

--- ci-build.sh
+++ ci-build.sh
@@ -5,39 +5,35 @@
 # Author: Qian Hong <fracting@gmail.com>
 
 # Functions
-if [[ -t 1 ]]; then
-    normal='\e[0m'
-    cyan='\e[1;36m'
-    green='\e[1;32m'
-    red='\e[1;31m'
-fi
-status()  { echo -e "\n${cyan}[MSYS2 CI]${normal} ${@}\n"; }
-success() { echo -e "\n${green}[MSYS2 CI] SUCCESS:${normal} ${@}.\n"; exit 0; }
-failure() { echo -e "\n${red}[MSYS2 CI] FAILURE:${normal} ${@}.\n"; exit 1; }
-gitconf() { test -n "$(git config ${1})" && return 0; git config --global "${1}" "${2}"; }
-execute() { status "${package:+$package: }${1}"; ${@:2} || failure "${package:+$package: }${1} failed"; }
+failure()       { colored; printf "\n${red}[MSYS2 CI] FAILURE:${normal} ${1}.\n\n"; exit 1; }
+success()       { colored; printf "\n${green}[MSYS2 CI] SUCCESS:${normal} ${1}.\n\n"; exit 0; }
+status()        { colored; printf "\n${cyan}[MSYS2 CI]${normal} ${1}\n\n"; printf "${2:+\t%s\n}" "${2:+${@:2}}"; }
+commits()       { printf "\n\t${1}\n"; list_changes --pretty=format:"%ai::[%h] %s" | awk -F:: '{print "\t"$2}'; }
+colored()       { [[ -t 1  && -z "${normal}" ]] || return; normal='\e[0m'; red='\e[1;31m'; green='\e[1;32m'; cyan='\e[1;36m'; }
+execute()       { cd "${package:-.}"; status "${package:+$package: }${1}"; ${@:2} || failure "${package:+$package: }${1} failed"; cd - > /dev/null; }
+git_config()    { test -n "$(git config ${1})" && return 0; git config --global "${1}" "${2}" || failure 'Could not configure Git for makepkg'; }
+list_changes()  { local since_master; while IFS= read -r line; do test -n "${line}" && since_master+=("${line}"); done <<< "$(git log "${@}" upstream/master.. | sort -u)"
+                  local from_head;    while IFS= read -r line; do test -n "${line}" && from_head+=("${line}");    done <<< "$(git log "${@}" HEAD^.. | sort -u)"
+                  [[ -n "${since_master[@]}" ]] && printf '%s\n' "${since_master[@]}" || printf '%s\n' "${from_head[@]}"; }
+
+# Configure
+cd "$(dirname "$0")"
+git_config user.email 'ci@msys2.org'
+git_config user.name  'MSYS2 Continuous Integration'
+git remote add -f upstream 'https://github.com/Alexpux/MINGW-packages'
 
 # Detect
-cd "$(dirname "$0")"
-commit_range="$(git log -1 --pretty=format:%P | cut -d' ' -f1)..HEAD"
-files=($(git show --pretty=format: --name-only ${commit_range} | sort -u))
+files=($(list_changes --pretty=format: --name-only))
 for file in "${files[@]}"; do [[ "${file}" = */PKGBUILD ]] && packages+=("${file%/PKGBUILD}"); done
 test -n "${files}"    || failure 'Could not detect changed files'
 test -z "${packages}" && success 'No changes in package recipes'
 
-# Prepare
-execute 'Upgrading the system' pacman --sync --refresh --refresh --sysupgrade --noconfirm --noprogressbar
-gitconf user.name  'MSYS2 Continuous Integration' || failure 'Could not configure Git for makepkg'
-gitconf user.email 'ci@msys2.org'                 || failure 'Could not configure Git for makepkg'
-
-# Build and install
-status 'Building changed packages:'
-for package in "${packages[@]}"; do printf "\t${package}\n"; done
+# Build
+status 'Building packages' "${packages[@]}"
+commits 'Changed by commits'
+execute 'Upgrading the system' pacman --noconfirm --noprogressbar --sync --refresh --refresh --sysupgrade
 for package in "${packages[@]}"; do
-    cd "${package}"
-    execute     'Installing dependencies' makepkg-mingw --skippgpcheck --noconfirm --noprogressbar --syncdeps --noprepare --nobuild
-    execute     'Building'                makepkg-mingw --skippgpcheck --noextract --nocheck
-    yes|execute 'Installing'              pacman --upgrade *.pkg.tar.xz
-    cd - > /dev/null
+    execute 'Building' makepkg-mingw --noconfirm --noprogressbar --skippgpcheck --nocheck --syncdeps --rmdeps --cleanbuild
+    yes|execute 'Installing' pacman --noprogressbar --upgrade *.pkg.tar.xz
 done
 success 'All packages built and installed successfully'
