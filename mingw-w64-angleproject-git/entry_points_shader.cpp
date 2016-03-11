84160040d2ab894b22438729d5a14fcbbc9af432
Martchus <martchus@gmx.net>
2016-03-11 20:21:54 +0100

    exported shader symbols
    - required to build Qt WebKit
    - see comment in PKGBUILD file for details

---
+++ mingw-w64-angleproject-git/entry_points_shader.cpp
@@ -0,0 +1,11 @@
+#ifndef COMPONENT_BUILD
+#define COMPONENT_BUILD
+#endif
+#ifdef ANGLE_TRANSLATOR_STATIC
+#undef ANGLE_TRANSLATOR_STATIC
+#endif
+#ifndef ANGLE_TRANSLATOR_IMPLEMENTATION
+#define ANGLE_TRANSLATOR_IMPLEMENTATION
+#endif
+
+#include "../compiler/translator/ShaderLang.cpp"
