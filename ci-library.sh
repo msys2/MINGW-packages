bca471a562eaa6d6c6e48a171b28bf7e42d7f9bb
Qian Hong <qhong@codeweavers.com>
2016-07-01 00:29:57 +0800

    CI: Use --needed when installing pactoys.

--- ci-library.sh
+++ ci-library.sh
@@ -134,7 +134,7 @@ update_system() {
     repman add ci.msys 'https://dl.bintray.com/alexpux/msys2' || return 1
     pacman --noconfirm --noprogressbar --sync --refresh --refresh --sysupgrade --sysupgrade || return 1
     test -n "${DISABLE_QUALITY_CHECK}" && return 0 # TODO: remove this option when not anymore needed
-    pacman --noconfirm --noprogressbar --sync ci.msys/pactoys
+    pacman --noconfirm --needed --noprogressbar --sync ci.msys/pactoys
 }
 
 # Sort packages by dependency
