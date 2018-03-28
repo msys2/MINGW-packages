05561ef550ba7aa0ed3b9347aaa160d35574d78d
Niklas Holm <jadedcyborg@gmail.com>
2018-03-28 11:30:55 +0200

    mingw-w64-perl: Package docs seperately
    - Package manpages and html documentation seperately in perl-doc as they
      are quite large and many users may not want them.
    - Generate the install scripts for each package from a template, as they
      are very similiar, instead of duplicating code.

--- mingw-w64-perl/setup_perl_dirs.sh
+++ mingw-w64-perl/setup_perl_dirs.sh
@@ -67,6 +67,8 @@ declare -i \
 ### No need to change anything below this line ###
 ###                                            ###
 
+null=''
+
 path() {
     if (( $3 == 0 )); then
         A='~'
@@ -80,7 +82,8 @@ path() {
     else
         P=''
     fi
-    printf "${A}INST_TOP${B}%s%s%s" \
+    printf "%s%s%s%s" \
+        "$([[ "${4:-}" == "doc" ]] && printf "${A}INST_DOC${B}" || printf "${A}INST_TOP${B}")" \
         "${!1}" \
         "$(if (( $2 >= 1 )); then
             if   [[ "$1" == *site ]]; then printf "${A}INST_SITE_VER${P}${B}"
@@ -92,55 +95,55 @@ path() {
 }
 
 sed -i -E \
--e "/^(install|site|vendor)?prefix(exp)?=/             s/=.*/='~INST_TOP~'/" \
 -e "/^d_((priv|arch)lib|(site|vendor)(arch|lib|bin|script))=/ s/=.*/='define'/" \
--e "/^libpth=/                            s/=.*/=''/" \
--e "/^installstyle=/                      s/=.*/='lib\/perl5'/" \
--e "/^perlpath=/                          s/=.*/='$(path  bin_core 2 0)"'\\perl.exe'"'/" \
--e "/^(install)?script(dir)?(exp)?=/      s/=.*/='$(path  scr_core 1 0)'/" \
--e "/^(install)?bin(exp)?=/               s/=.*/='$(path  bin_core 2 0)'/" \
--e "/^(install)?sitescript(exp)?=/        s/=.*/='$(path  scr_site 1 0)'/" \
--e "/^(install)?sitebin(exp)?=/           s/=.*/='$(path  bin_site 2 0)'/" \
--e "/^(install)?vendorscript(exp)?=/      s/=.*/='$(path  scr_vend 1 0)'/" \
--e "/^(install)?vendorbin(exp)?=/         s/=.*/='$(path  bin_vend 2 0)'/" \
--e "/^(install)?privlib(exp)?=/           s/=.*/='$(path priv_core 1 0)'/" \
--e "/^(install)?archlib(exp)?=/           s/=.*/='$(path arch_core 2 0)'/" \
--e "/^(install)?sitelib_stem=/            s/=.*/='$(path priv_site 0 0)'/" \
--e "/^(install)?sitelib(exp)?=/           s/=.*/='$(path priv_site 1 0)'/" \
--e "/^(install)?sitearch(exp)?=/          s/=.*/='$(path arch_site 2 0)'/" \
--e "/^(install)?vendorlib_stem=/          s/=.*/='$(path priv_vend 0 0)'/" \
--e "/^(install)?vendorlib(exp)?=/         s/=.*/='$(path priv_vend 1 0)'/" \
--e "/^(install)?vendorarch(exp)?=/        s/=.*/='$(path arch_vend 2 0)'/" \
--e "/^(install)?htmldir(exp)?=/           s/=.*/='$(path html      1 0)'/" \
--e "/^(install)?htmlhelpdir(exp)?=/       s/=.*/='$(path html      1 0)"'\\htmlhelp'"'/" \
--e "/^(install)?html[13]dir(exp)?=/       s/=.*/='$(path html_core 1 0)'/" \
--e "/^(install)?sitehtml[13]dir(exp)?=/   s/=.*/='$(path html_site 1 0)'/" \
--e "/^(install)?vendorhtml[13]dir(exp)?=/ s/=.*/='$(path html_vend 1 0)'/" \
--e "/^(install)?(vendor)?man1dir(exp)?=/  s/=.*/='~INST_TOP~"'\\share\\man\\man1'"'/" \
--e "/^(install)?(vendor)?man3dir(exp)?=/  s/=.*/='~INST_TOP~"'\\share\\man\\man3'"'/" \
--e "/^(install)?siteman1dir(exp)?=/       s/=.*/='~INST_TOP~"'\\local\\man\\man1'"'/" \
--e "/^(install)?siteman3dir(exp)?=/       s/=.*/='~INST_TOP~"'\\local\\man\\man3'"'/" \
--e "/^man1ext=/                           s/=.*/='1perl'/" \
--e "/^man3ext=/                           s/=.*/='3perl'/" \
--e "/^usevendorprefix=/                   s/=.*/='define'/" \
--e "/^otherlibdirs=/                      s/=.*/=' '/" \
--e "/^d_perl_otherlibdirs=/               s/=.*/='undef'/" \
--e "/^inc_version_list=/                  s/=.*/=' '/" \
--e "/^inc_version_list_init=/             s/=.*/='0'/" \
--e "/^d_inc_version_list=/                s/=.*/='undef'/" \
--e "/^cf_by=/                             s/=.*/=' '/" \
--e "/^cf_email=/                          s/=.*/=' '/" \
--e "/^perladmin=/                         s/=.*/=' '/" \
+-e "/^(install|site|vendor)?prefix(exp)?=/ s/=.*/='~INST_TOP~'/" \
+-e "/^libpth=/                             s/=.*/=''/" \
+-e "/^installstyle=/                       s/=.*/='lib\/perl5'/" \
+-e "/^perlpath=/                           s/=.*/='$(path  bin_core 2 0)"'\\perl.exe'"'/" \
+-e "/^(install)?script(dir)?(exp)?=/       s/=.*/='$(path  scr_core 1 0)'/" \
+-e "/^(install)?bin(exp)?=/                s/=.*/='$(path  bin_core 2 0)'/" \
+-e "/^(install)?sitescript(exp)?=/         s/=.*/='$(path  scr_site 1 0)'/" \
+-e "/^(install)?sitebin(exp)?=/            s/=.*/='$(path  bin_site 2 0)'/" \
+-e "/^(install)?vendorscript(exp)?=/       s/=.*/='$(path  scr_vend 1 0)'/" \
+-e "/^(install)?vendorbin(exp)?=/          s/=.*/='$(path  bin_vend 2 0)'/" \
+-e "/^(install)?privlib(exp)?=/            s/=.*/='$(path priv_core 1 0)'/" \
+-e "/^(install)?archlib(exp)?=/            s/=.*/='$(path arch_core 2 0)'/" \
+-e "/^(install)?sitelib_stem=/             s/=.*/='$(path priv_site 0 0)'/" \
+-e "/^(install)?sitelib(exp)?=/            s/=.*/='$(path priv_site 1 0)'/" \
+-e "/^(install)?sitearch(exp)?=/           s/=.*/='$(path arch_site 2 0)'/" \
+-e "/^(install)?vendorlib_stem=/           s/=.*/='$(path priv_vend 0 0)'/" \
+-e "/^(install)?vendorlib(exp)?=/          s/=.*/='$(path priv_vend 1 0)'/" \
+-e "/^(install)?vendorarch(exp)?=/         s/=.*/='$(path arch_vend 2 0)'/" \
+-e "/^(install)?htmldir(exp)?=/            s/=.*/='$(path html      1 0 doc)'/" \
+-e "/^(install)?htmlhelpdir(exp)?=/        s/=.*/='$(path html      1 0 doc)"'\\htmlhelp'"'/" \
+-e "/^(install)?html[13]dir(exp)?=/        s/=.*/='$(path html_core 1 0 doc)'/" \
+-e "/^(install)?sitehtml[13]dir(exp)?=/    s/=.*/='$(path html_site 1 0 doc)'/" \
+-e "/^(install)?vendorhtml[13]dir(exp)?=/  s/=.*/='$(path html_vend 1 0 doc)'/" \
+-e "/^(install)?(vendor)?man1dir(exp)?=/   s/=.*/='$(path null 0 0 doc)"'\\share\\man\\man1'"'/" \
+-e "/^(install)?(vendor)?man3dir(exp)?=/   s/=.*/='$(path null 0 0 doc)"'\\share\\man\\man3'"'/" \
+-e "/^(install)?siteman1dir(exp)?=/        s/=.*/='$(path null 0 0 doc)"'\\local\\man\\man1'"'/" \
+-e "/^(install)?siteman3dir(exp)?=/        s/=.*/='$(path null 0 0 doc)"'\\local\\man\\man3'"'/" \
+-e "/^man1ext=/                            s/=.*/='1perl'/" \
+-e "/^man3ext=/                            s/=.*/='3perl'/" \
+-e "/^usevendorprefix=/                    s/=.*/='define'/" \
+-e "/^otherlibdirs=/                       s/=.*/=' '/" \
+-e "/^d_perl_otherlibdirs=/                s/=.*/='undef'/" \
+-e "/^inc_version_list=/                   s/=.*/=' '/" \
+-e "/^inc_version_list_init=/              s/=.*/='0'/" \
+-e "/^d_inc_version_list=/                 s/=.*/='undef'/" \
+-e "/^cf_by=/                              s/=.*/=' '/" \
+-e "/^cf_email=/                           s/=.*/=' '/" \
+-e "/^perladmin=/                          s/=.*/=' '/" \
 "${srcdir}"/win32/config.gc
 
 # We have no (site|vendor)man(1|3)ext, to get the same effect, change man(1|3)ext after make install
 
 sed -i -E \
--e "/^INST_SCRIPT\s+=/                  s/=.*/= $(path  scr_core 1 1)/" \
--e "/^INST_BIN\s+=/                     s/=.*/= $(path  bin_core 2 1)/" \
--e "/^INST_LIB\s+=/                     s/=.*/= $(path priv_core 1 1)/" \
--e "/^INST_ARCHLIB\s+=/                 s/=.*/= $(path arch_core 2 1)/" \
--e "/^INST_HTML\s+=/                    s/=.*/= $(path html      1 1)/" \
+-e "/^INST_SCRIPT\s+=/                     s/=.*/= $(path  scr_core 1 1)/" \
+-e "/^INST_BIN\s+=/                        s/=.*/= $(path  bin_core 2 1)/" \
+-e "/^INST_LIB\s+=/                        s/=.*/= $(path priv_core 1 1)/" \
+-e "/^INST_ARCHLIB\s+=/                    s/=.*/= $(path arch_core 2 1)/" \
+-e "/^INST_HTML\s+=/                       s/=.*/= $(path html      1 1 doc)/" \
 -e "s/^#?(INST_VER\s)/\1/" \
 -e "s/^#?(INST_CORE_VER\s)/$( ((CORE_VER)) || echo -n '#')\1/" \
 -e "s/^#?(INST_SITE_VER\s)/$( ((SITE_VER)) || echo -n '#')\1/" \
