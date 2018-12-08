7506b84859b85c2a93cfdbe7325d310e90403b37
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-12-08 20:15:44 +0100

    git: fix indentation in git-wrapper.c
    We try to use (8-column) tabs for indentation.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- mingw-w64-git/git-wrapper.c
+++ mingw-w64-git/git-wrapper.c
@@ -43,31 +43,31 @@ static void print_error(LPCWSTR prefix, DWORD error_number)
 
 
 static void find_exe_realpath(LPWSTR exepath, int count) {
-  WCHAR mdexe[MAX_PATH], realexe[MAX_PATH];
-  HANDLE hFile = INVALID_HANDLE_VALUE;
-  DWORD dwlen = 0;
-  /* get the installation location */
-  GetModuleFileNameW(NULL, mdexe, MAX_PATH);
-  hFile = CreateFileW(mdexe,                 /* file to open */
-                      GENERIC_READ,          /* open for reading */
-                      FILE_SHARE_READ,       /* share for reading */
-                      NULL,                  /* default security */
-                      OPEN_EXISTING,         /* existing file only */
-                      FILE_ATTRIBUTE_NORMAL, /* normal file */
-                      NULL);                 /* no attr. template */
-  if (hFile == INVALID_HANDLE_VALUE) {
-    wcscpy(exepath, mdexe);
-    return;
-  }
-  dwlen = GetFinalPathNameByHandleW(hFile, realexe, MAX_PATH, VOLUME_NAME_DOS);
-  if (dwlen >= MAX_PATH) {
-    wcscpy(exepath, mdexe);
-  } else {
-    wcsncpy(exepath, realexe + 4, dwlen - 4);
-  }
-  CloseHandle(hFile);
+	WCHAR mdexe[MAX_PATH], realexe[MAX_PATH];
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+	DWORD dwlen = 0;
+	/* get the installation location */
+	GetModuleFileNameW(NULL, mdexe, MAX_PATH);
+	hFile = CreateFileW(mdexe,                 /* file to open */
+			    GENERIC_READ,          /* open for reading */
+			    FILE_SHARE_READ,       /* share for reading */
+			    NULL,                  /* default security */
+			    OPEN_EXISTING,         /* existing file only */
+			    FILE_ATTRIBUTE_NORMAL, /* normal file */
+			    NULL);                 /* no attr. template */
+	if (hFile == INVALID_HANDLE_VALUE) {
+		wcscpy(exepath, mdexe);
+		return;
+	}
+	dwlen = GetFinalPathNameByHandleW(hFile, realexe, MAX_PATH,
+					  VOLUME_NAME_DOS);
+	if (dwlen >= MAX_PATH) {
+		wcscpy(exepath, mdexe);
+	} else {
+		wcsncpy(exepath, realexe + 4, dwlen - 4);
+	}
+	CloseHandle(hFile);
 }
-
 
 static void my_path_append(LPWSTR list, LPCWSTR path, size_t alloc)
 {
