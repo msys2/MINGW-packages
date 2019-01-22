885aaee4992a529c613c88412e2ea858e111c72a
ofry <tim4job@bmail.ru>
2019-01-22 22:41:42 +0300

    Try to create GHC 8.6.3 build script. Also fixed https://github.com/Alexpux/MINGW-packages/issues/4800
    but build still fails later (on stage 2)
    
    Signed-off-by: ofry <tim4job@bmail.ru>

--- mingw-w64-ghc/build.mk
+++ mingw-w64-ghc/build.mk
@@ -61,7 +61,7 @@ endif
 
 # Set to V = 0 to get prettier build output.
 # Please use V=1 (the default) when reporting GHC bugs.
-V=0
+V=1
 
 # Should all enabled warnings (see mk/warnings.mk) be turned into errors while
 # building stage 2?
