57fb7bce5c4f528256fc09abc69619566aaf2d87
Oleg A. Khlybov <fougas@mail.ru>
2019-10-24 04:06:06 -0700

    Added debugging output for buildtest launcher. Added Path environment augmentation when run outside the MSYS shell.

--- mingw-w64-buildtest/buildtests.c
+++ mingw-w64-buildtest/buildtests.c
@@ -31,8 +31,8 @@ int main(int argc, char** argv, char** envp) {
 	char* tmpdir = malloc(BUFFER_SIZE*sizeof(char)); assert(tmpdir);
 	char* buffer = malloc(BUFFER_SIZE*sizeof(char)); assert(buffer);
 	GetTempPath(BUFFER_SIZE-1, buffer);
-	srand((unsigned)time(NULL));
 	// Create unique temp dir
+	srand((unsigned)time(NULL));
 	do {snprintf(tmpdir, BUFFER_SIZE, "%s\\buildtests%d", buffer, (unsigned short)hash(rand()));} while(!CreateDirectory(tmpdir, NULL));
 	// Fix environment to set to new temp dir
 	for(i = 0; envp[i]; ++i) {
@@ -52,8 +52,10 @@ int main(int argc, char** argv, char** envp) {
 			envp[i] = env;
 			continue;
 		}
-#if 0
-		#define T "PATH="
+#if 1
+		// Augment Windows-specific Path variable with root directory
+		// When run from within the MSYS2's MingGW shell the PATH variable is augmented automatically
+		#define T "Path="
 		if(strncmp(T, envp[i], strlen(T)) == 0) {
 			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
 			strcpy(buffer, &envp[i][strlen(T)]);
@@ -68,25 +70,36 @@ int main(int argc, char** argv, char** envp) {
 	char* rmpath = malloc(BUFFER_SIZE*sizeof(char)); assert(rmpath);
 	snprintf(rmpath, BUFFER_SIZE, "%s\\..\\libexec\\rmpath.exe", root);
 	// Construct args
-	i = 0;
 	char** args = malloc(BUFFER_SIZE*sizeof(char*)); assert(args);
 	// Set path to tclsh executable
 	char* tclsh = malloc(BUFFER_SIZE*sizeof(char)); assert(tclsh);
 	snprintf(tclsh, BUFFER_SIZE, "%s\\tclsh.exe", root);
+	i = 0;
 	args[i++] = tclsh;
 	// Set path to init script
 	char* all_tcl = malloc(BUFFER_SIZE*sizeof(char)); assert(all_tcl);
 	snprintf(all_tcl, BUFFER_SIZE, "%s\\..\\share\\tests\\all.tcl", root);
 	args[i++] = all_tcl;
 	// Copy remaining args
-	for(int argvi = 1; argvi < argc; ++argvi) {
-		args[i++] = argv[argvi];
+	for(int ii = 1; ii < argc; ++ii) {
+		args[i++] = argv[ii];
 	}
 	args[i] = NULL;
-  SetDllDirectory(root);
+#ifndef NDEBUG
+	puts("\n");
+	for(i = 0; envp[i]; ++i) printf("%s\n", envp[i]);
+	printf("\n_spawn(_P_WAIT): ");
+	for(i = 0; args[i]; ++i) printf("%s ", args[i]);
+	puts("\n");
+	fflush(stdout);
+#endif
 	// Execute tests
 	int c = _spawnve(_P_WAIT, tclsh, args, envp);
 	// Fire up cleaning process
+#ifndef NDEBUG
+	printf("\n_spawn(_P_NOWAIT): %s %s\n", rmpath, tmpdir);
+	fflush(stdout);
+#endif
 	_spawnl(_P_NOWAIT, rmpath, rmpath, tmpdir, NULL);
 	return c;
 }	
