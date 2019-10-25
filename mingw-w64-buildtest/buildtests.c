5d437c126cc4a63bf99255ccba846f891a1fc9d7
Oleg A. Khlybov <fougas@mail.ru>
2019-10-25 07:16:32 -0700

    Added staging directory cleanup command-line argument. Added code for command-line options filtration.

--- mingw-w64-buildtest/buildtests.c
+++ mingw-w64-buildtest/buildtests.c
@@ -6,15 +6,9 @@
 #include <shellapi.h>
 #include <inttypes.h>
 
-#ifdef _WIN64
-#define BIT "64"
-#else
-#define BIT "32"
-#endif
-
 #define BUFFER_SIZE 32767
 
-uint32_t hash( uint32_t a) {
+static uint32_t hash( uint32_t a) {
     a = (a ^ 61) ^ (a >> 16);
     a = a + (a << 3);
     a = a ^ (a >> 4);
@@ -34,38 +28,6 @@ int main(int argc, char** argv, char** envp) {
 	// Create unique temp dir
 	srand((unsigned)time(NULL));
 	do {snprintf(tmpdir, BUFFER_SIZE, "%s\\buildtests%d", buffer, (unsigned short)hash(rand()));} while(!CreateDirectory(tmpdir, NULL));
-	// Fix environment to set to new temp dir
-	for(i = 0; envp[i]; ++i) {
-		#undef T
-		#define T "TMP="
-		if(strncmp(T, envp[i], strlen(T)) == 0) {
-			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
-			snprintf(env, BUFFER_SIZE, "%s%s", T, tmpdir);
-			envp[i] = env;
-			continue;
-		}
-		#undef T
-		#define T "TEMP="
-		if(strncmp(T, envp[i], strlen(T)) == 0) {
-			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
-			snprintf(env, BUFFER_SIZE, "%s%s", T, tmpdir);
-			envp[i] = env;
-			continue;
-		}
-#if 1
-		// Augment Windows-specific Path variable with root directory
-		// When run from within the MSYS2's MingGW shell the PATH variable gets updated automatically
-		#undef T
-		#define T "Path="
-		if(strncmp(T, envp[i], strlen(T)) == 0) {
-			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
-			strcpy(buffer, &envp[i][strlen(T)]);
-			snprintf(env, BUFFER_SIZE, "%s%s;%s", T, root, buffer);
-			envp[i] = env;
-			continue;
-		}
-#endif
-	}
 	// Path remover executable
 	char* rmpath = malloc(BUFFER_SIZE*sizeof(char)); assert(rmpath);
 	snprintf(rmpath, BUFFER_SIZE, "%s\\..\\libexec\\rmpath.exe", root);
@@ -80,26 +42,91 @@ int main(int argc, char** argv, char** envp) {
 	char* all_tcl = malloc(BUFFER_SIZE*sizeof(char)); assert(all_tcl);
 	snprintf(all_tcl, BUFFER_SIZE, "%s\\..\\share\\tests\\all.tcl", root);
 	args[i++] = all_tcl;
+	int cleanup = 1; // Should staging directory be deleted after the tests execution.
 	// Copy remaining args
 	for(int ii = 1; ii < argc; ++ii) {
+		// Detect and remove flags meaningless to the tcltest.
+		if(strcmp("-nocleanup", argv[ii]) == 0) {
+#ifndef NDEBUG
+			printf("*** Staging directory cleanup disabled with -nocleanup command-line option\n");
+#endif
+			cleanup = 0;
+			continue;
+		}
 		args[i++] = argv[ii];
 	}
 	args[i] = NULL;
+	// Filter environment to set to new temp & path.
+	i = 0;
+	char** envs = malloc(BUFFER_SIZE*sizeof(char*)); assert(envs);
+	for(int ii = 0; envp[ii]; ++ii) {
+		#undef T
+		#define T "TMP="
+		if(strncmp(T, envp[ii], strlen(T)) == 0) {
+			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
+			snprintf(env, BUFFER_SIZE, "%s%s", T, tmpdir);
+			envs[i++] = env;
+			continue;
+		}
+		#undef T
+		#define T "TEMP="
+		if(strncmp(T, envp[ii], strlen(T)) == 0) {
+			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
+			snprintf(env, BUFFER_SIZE, "%s%s", T, tmpdir);
+			envs[i++] = env;
+			continue;
+		}
+		// Native Windows cmd and MinGW shell set different path variables.
+		// Augment whatever path variable found.
+		#undef T
+		#define T "PATH="
+		if(strncmp(T, envp[ii], strlen(T)) == 0) {
+			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
+			strncpy(buffer, BUFFER_SIZE, &envp[i][strlen(T)]);
+			snprintf(env, BUFFER_SIZE, "%s%s;%s", T, root, buffer);
+			envs[i++] = env;
+			continue;
+		}
+		#undef T
+		#define T "Path="
+		if(strncmp(T, envp[ii], strlen(T)) == 0) {
+			char* env = malloc(BUFFER_SIZE*sizeof(char)); assert(env);
+			strncpy(buffer, BUFFER_SIZE, &envp[i][strlen(T)]);
+			snprintf(env, BUFFER_SIZE, "%s%s;%s", T, root, buffer);
+			envs[i++] = env;
+			continue;
+		}
+		//
+		envs[i++] = envp[ii];
+	}
+	{
+		char* clup = malloc(BUFFER_SIZE*sizeof(char)); assert(clup);
+		snprintf(clup, BUFFER_SIZE, "BUILDTEST_CLEANUP=%d", cleanup);
+		envs[i++] = clup;
+	}
+	envs[i] = NULL;
 #ifndef NDEBUG
 	puts("\n");
-	for(i = 0; envp[i]; ++i) printf("%s\n", envp[i]);
+	for(i = 0; envs[i]; ++i) printf("%s\n", envs[i]);
 	printf("\n_spawn(_P_WAIT): ");
 	for(i = 0; args[i]; ++i) printf("%s ", args[i]);
 	puts("\n");
 	fflush(stdout);
 #endif
-	// Execute tests
-	int c = _spawnve(_P_WAIT, tclsh, args, envp);
-	// Fire up cleaning process
+	// Execute tests.
+	int c = _spawnve(_P_WAIT, tclsh, args, envs);
+	// Fire up cleaning process.
+	if(cleanup) {
 #ifndef NDEBUG
-	printf("\n_spawn(_P_NOWAIT): %s %s\n", rmpath, tmpdir);
-	fflush(stdout);
+		printf("\n_spawn(_P_NOWAIT): %s %s\n", rmpath, tmpdir);
+		fflush(stdout);
 #endif
-	_spawnl(_P_NOWAIT, rmpath, rmpath, tmpdir, NULL);
+		_spawnl(_P_NOWAIT, rmpath, rmpath, tmpdir, NULL);
+	} else {
+#ifndef NDEBUG
+		printf("\nPreserving staging directory %s\n", tmpdir);
+		fflush(stdout);
+#endif
+	}
 	return c;
 }	
