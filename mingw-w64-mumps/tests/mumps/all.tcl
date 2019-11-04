62da9b4a320b3c9cc2d9b4574f0acac068de0f01
Oleg A. Khlybov <fougas@mail.ru>
2019-11-05 01:45:28 +0500

    Added build & run test suite.

---
+++ mingw-w64-mumps/tests/mumps/all.tcl
@@ -0,0 +1,6 @@
+# Template `all.tcl` file which is to be put into each test module's directory
+package require tcltest
+namespace import ::tcltest::*
+configure -testdir [file dirname [file normalize [info script]]] {*}$argv 
+runAllTests
+cleanupTests
