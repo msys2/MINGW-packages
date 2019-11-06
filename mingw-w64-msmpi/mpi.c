795efed868685338b3bd49a7742ddb7b6f990354
Oleg A. Khlybov <fougas@mail.ru>
2019-11-06 16:57:02 +0500

    New package: msmpi - Microsoft MPI SDK

---
+++ mingw-w64-msmpi/mpi.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+
+#if defined(CC)
+	#define C "gcc.exe"
+	#define E "MPICC"
+#elif defined(CXX)
+	#define C "g++.exe"
+	#define E "MPICXX"
+#elif defined(FC)
+	#define C "gfortran.exe"
+	#define E "MPIFORT"
+#else
+	#error
+#endif
+
+int main(int argc, char** argv) {
+	int s = strlen(argv[0]);
+	while(argv[0][s] != '\\' && argv[0][s] != '/') --s; --s;
+	while(argv[0][s] != '\\' && argv[0][s] != '/') --s;
+	char slash = argv[0][s];
+	argv[0][s] = '\0';
+#define SZ 32767	
+	char* lpath = malloc(SZ*sizeof(char));
+	snprintf(lpath, SZ, "-L%s%clib", argv[0], slash);
+	char* ipath = malloc(SZ*sizeof(char));
+	snprintf(ipath, SZ, "-I%s%cinclude", argv[0], slash);
+	char* comp = malloc(SZ*sizeof(char));
+	char** args = malloc(SZ*sizeof(char*));
+	int show = (argc == 2 && strcmp(argv[1], "-show") == 0);
+	int i = 0;
+	args[i++] = GetEnvironmentVariable(E, comp, SZ) > 0 ? comp : C;
+	args[i++] = ipath;
+#if 0
+// Not needed for anymore MS-MPI 10+
+#ifdef FC
+	args[i++] = "-fno-range-check";
+#else	
+	args[i++] = "-include"; args[i++] = "inttypes.h";
+#endif
+#endif
+	if(!show) for(int x = 1; x < argc; ++x) args[i++] = argv[x];
+	args[i++] = lpath;
+	args[i++] = "-lmsmpi";
+	args[i] = NULL;
+	if(show) {
+		for(int x = 0; args[x]; ++x) printf("%s ", args[x]);
+		printf("\n");
+		fflush(stdout);
+		return 0;
+	} else {
+		return _spawnvp(_P_WAIT, args[0], args);
+	}
+};
