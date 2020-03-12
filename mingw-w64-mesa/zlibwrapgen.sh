2b8701450568b6749f9c02fa4ef95483c129861b
pal1000 <liviuprodea@yahoo.com>
2020-03-12 22:08:34 +0200

    mingw-w64-mesa: Get static linking right. Drop all runtime dependencies. This makes Meson behave exactly like Scons did before replacement. Drop shared build. I didn't see much value in it anyway, especially with both linking negating all storage saving and the fact that there are no packages in mingw-packages and msys2-packages repositories depending on mingw-w64-mesa at this point. mingw-w64-mesa has been fine with static linking since its debut anyway.

---
+++ mingw-w64-mesa/zlibwrapgen.sh
@@ -0,0 +1,21 @@
+# Generate a Meson wrap file for zlib
+mkdir -p ./subprojects/zlib
+FILE="./subprojects/zlib/meson.build"
+/bin/cat <<EOM >${FILE}
+project('zlib', ['cpp'])
+
+cpp = meson.get_compiler('cpp')
+
+_deps = []
+_search = '$(cygpath -m ${MINGW_PREFIX})/lib'
+foreach d : ['libz', 'libz.dll']
+  _deps += cpp.find_library(d, dirs : _search)
+endforeach
+
+zlib_dep = declare_dependency(
+  include_directories : include_directories('$(cygpath -m ${MINGW_PREFIX})/include'),
+  dependencies : _deps,
+  version : '$(pacman -Q ${MINGW_PACKAGE_PREFIX}-zlib | cut -d" " -f 2 | cut -d"-" -f 1)',
+)
+
+EOM
