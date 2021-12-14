b91d002a690919d6a0157c0aea2eb1c82482e438
Christoph Reiter <reiter.christoph@gmail.com>
2021-12-14 22:36:03 +0100

    autotools template: use mingw autotools variant

--- mingw-w64-PKGBUILD-templates/PKGBUILD.autotools-tarball.sh
+++ mingw-w64-PKGBUILD-templates/PKGBUILD.autotools-tarball.sh
@@ -9,7 +9,7 @@ pkgdesc="Some package (mingw-w64)"
 arch=('any')
 url='https://www.somepackage.org/'
 license=('LICENSE')
-makedepends=('autotools')
+makedepends=("${MINGW_PACKAGE_PREFIX}-autotools")
 source=("https://www.somepackage.org/${_realname}/${_realname}-${pkgver}.tar.gz"
         "0001-A-fix.patch")
 sha256sums=('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
