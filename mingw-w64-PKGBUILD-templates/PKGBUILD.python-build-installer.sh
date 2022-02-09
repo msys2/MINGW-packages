6252dafa152bb548aa3303736107f51fbaab78e0
MARINA\jpmug <jpmugaas@suddenlink.net>
2022-02-09 12:15:37 -0500

    address some concerns expressed by  lazka .

--- mingw-w64-PKGBUILD-templates/PKGBUILD.python-build-installer.sh
+++ mingw-w64-PKGBUILD-templates/PKGBUILD.python-build-installer.sh
@@ -10,8 +10,8 @@ arch=('any')
 mingw_arch=('mingw32' 'mingw64' 'ucrt64' 'clang64' 'clang32' 'clangarm64')
 url='https://www.somepackage.org/'
 license=('LICENSE')
-depends=("${MINGW_PACKAGE_PREFIX}-python" "${MINGW_PACKAGE_PREFIX}-python-installer")
-makedepends=("${MINGW_PACKAGE_PREFIX}-python-build")
+depends=("${MINGW_PACKAGE_PREFIX}-python")
+makedepends=("${MINGW_PACKAGE_PREFIX}-python-build" "${MINGW_PACKAGE_PREFIX}-python-installer")
 checkdepends=("${MINGW_PACKAGE_PREFIX}-python-pytest")
 source=("https://pypi.org/packages/source/${_realname::1}/${_realname}/${_realname}-${pkgver}.tar.gz"
         "0001-An-important-fix.patch"
@@ -52,16 +52,9 @@ package() {
   msg "Python install for ${MSYSTEM}"
   cd "${srcdir}/python-build-${MSYSTEM}"
 
-  MSYS2_ARG_CONV_EXCL="--prefix=;--install-scripts=;--install-platlib=" \
+  MSYS2_ARG_CONV_EXCL="--prefix=" \
     ${MINGW_PREFIX}/bin/python -m installer --prefix=${MINGW_PREFIX} \
     --destdir="${pkgdir}" dist/*.whl
 
   install -Dm644 LICENSE "${pkgdir}${MINGW_PREFIX}/share/licenses/python-${_realname}/LICENSE"
-
-  ## (OPTIONAL) This entire section should be removed
-  ## if the package does NOT instal anything in the ${MINGW_PREFIX}/bin directory.
-  for _f in "${pkgdir}${MINGW_PREFIX}"/bin/*-script.py; do
-    # Remove shebang line
-    sed -e '1 { s/^#!.*$// }' -i "${_f}"
-  done
 }
