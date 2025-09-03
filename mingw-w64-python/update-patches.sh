31b57a0f7afeb3daca8471703c77f649a6153476
Christoph Reiter <reiter.christoph@gmail.com>
2025-09-03 08:58:04 +0200

    python: fix exluding CI patches
    some syntax typos leading to it being ignored

--- mingw-w64-python/update-patches.sh
+++ mingw-w64-python/update-patches.sh
@@ -50,8 +50,8 @@ git -c core.abbrev=7 \
 		--suffix=.patch \
 		--subject-prefix=PATCH \
 		--output-directory .. \
-			$base_tag..$msys2_branch ||
-			-- ':(exclude).github/' ||
+		$base_tag..$msys2_branch \
+		-- ':(exclude).github/' ||
 die "Could not generate new patch set"
 
 patches="$(ls -1 0*.patch)" &&
