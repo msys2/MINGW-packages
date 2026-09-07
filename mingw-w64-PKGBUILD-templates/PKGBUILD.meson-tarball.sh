1cc9adc0598d517ae67424e9ff3fe7e5769c4858
Zhongteng Gui <dragon-archer@outlook.com>
2026-09-07 13:59:02 +0800

    replace pkg-config with pkgconf everywhere

--- mingw-w64-PKGBUILD-templates/PKGBUILD.meson-tarball.sh
+++ mingw-w64-PKGBUILD-templates/PKGBUILD.meson-tarball.sh
@@ -12,7 +12,7 @@ url='https://www.somepackage.org/'
 license=('LICENSE')
 makedepends=("${MINGW_PACKAGE_PREFIX}-meson"
              "${MINGW_PACKAGE_PREFIX}-ninja"
-             "${MINGW_PACKAGE_PREFIX}-pkg-config"
+             "${MINGW_PACKAGE_PREFIX}-pkgconf"
              "${MINGW_PACKAGE_PREFIX}-cc")
 source=("https://www.somepackage.org/${_realname}/${_realname}-${pkgver}.tar.gz"
         "0001-An-important-fix.patch"
